4b317fdb141582a91935bd280dafcaf90a876024
Enigma-I-am <nwagba001@gmail.com>
2023-08-23 04:59:37 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.dera_x_neddy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:dera_x_neddy/app/app.dart';
+import 'package:dera_x_neddy/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
